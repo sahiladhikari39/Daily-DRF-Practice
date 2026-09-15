52e881d55e693f1ae0c0d28501637da68b12e4da
Sahil Adhikari <sahiladhi39@gmail.com>
2026-09-15 10:38:04 +0545

    Learned post in generic views

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Product_Detail_view, Product_View
+from .views import Product_Detail_view, Product_View, Product_create
 
 
 urlpatterns = [
     # path('products/', product_detail),
     path('products/', Product_View.as_view()),
+    path('products/create', Product_create.as_view()),
     path('products/<int:pk>', Product_Detail_view.as_view()),
     
 ]
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.generics import GenericAPIView
+from rest_framework import status
 # Create your views here.
 
 # @api_view(['GET'])
@@ -20,6 +21,18 @@ class Product_View(GenericAPIView):
         product = self.get_queryset()
         serializer = self.get_serializer(product, many =True)
         return Response(serializer.data)
+
+
+class Product_create(GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class Product_Detail_view(GenericAPIView):
