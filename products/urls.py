dccde0264b60425d3ab5d82e2d7f3a9a6648feb8
Sahil Adhikari <sahiladhi39@gmail.com>
2026-09-15 14:52:25 +0545

    learned about GenericAPIView + Mixins

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import Product_Detail_view, Product_List, Product_create
+from .views import ProductListCreateView, ProductUpdateView, ProductDeleteView
 
 
 urlpatterns = [
     # path('products/', product_detail),
-    path('products/', Product_List.as_view()),
-    path('products/<int:pk>', Product_List.as_view()),
-    path('products/create', Product_create.as_view()),
-    path('products/<int:pk>', Product_Detail_view.as_view()),
-    
+    # path('products/', Product_List.as_view()),
+    # path('products/<int:pk>', Product_List.as_view()),
+    # path('products/create', Product_create.as_view()),
+    # path('products/<int:pk>', Product_Detail_view.as_view()),
+
+    path('products/', ProductListCreateView.as_view()),
+    path('products/<int:pk>', ProductUpdateView.as_view()),
+    path('products/del/<int:pk>', ProductDeleteView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
 # Create your views here.
 
 # @api_view(['GET'])
@@ -13,56 +14,83 @@ from rest_framework import status
 #     return Response(serializer.data)
 
 
-class Product_List(GenericAPIView):
+# class Product_List(GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     def get(self, request):
+#         product = self.get_queryset()
+#         serializer = self.get_serializer(product, many=True)
+#         return Response(serializer.data)
+    
+#     def put(self, request, pk):
+#         product = self.get_object()
+#         serializer = self.get_serializer(product, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def patch(self, request, pk):
+#         product = self.get_object()
+#         serializer = self.get_serializer(product, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         product = self.get_object()
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# class Product_create(GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     def post(self, request):
+#         serializer = self.get_serializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class Product_Detail_view(GenericAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     def get(self, request, pk):
+#         product = self.get_object()
+#         serializer = self.get_serializer(product)
+#         return Response(serializer)
+
+
+class ProductListCreateView(ListModelMixin, CreateModelMixin, GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get(self, request):
-        product = self.get_queryset()
-        serializer = self.get_serializer(product, many=True)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        product = self.get_object()
-        serializer = self.get_serializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk):
-        product = self.get_object()
-        serializer = self.get_serializer(product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        product = self.get_object()
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class Product_create(GenericAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+        return self.list(request)
 
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return self.create(request)
 
 
-class Product_Detail_view(GenericAPIView):
+class ProductUpdateView(UpdateModelMixin, GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def get(self, request, pk):
-        product = self.get_object()
-        serializer = self.get_serializer(product)
-        return Response(serializer)
+    def put(self, request, pk):
+        return self.update(request, pk=pk)
 
+    def patch(self, request, pk):
+        return self.partial_update(request, pk=pk)
 
+class ProductDeleteView(DestroyModelMixin, GenericAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk=pk)
